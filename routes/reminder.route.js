6001b84710f278aef773b979a06e65590717bee7
Imad Ali <imada2435@gmail.com>
2025-10-20 23:35:08 -0700

    Revert "Complete backend of  Reminder System (CRUD + Scheduler + Email Notifications)"

--- routes/reminder.route.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { createReminder, updateReminder, deleteReminder ,getReminders } from "../controller/Reminder.conroller.js";
-const router= express.Router();
-router.post('/create',createReminder);
-router.get('/get', getReminders);
-router.put('/update/:id', updateReminder);
-router.delete('/delete/:id',deleteReminder)
-export default router;
